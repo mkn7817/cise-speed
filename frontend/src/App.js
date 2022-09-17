c4b4c65f856099111bd9e8a855a0967d86f89ecf
mkn7817 <mkn7817@autuni.ac.nz>
2022-09-17 21:17:43 +1200

    adding frontend components, CreateArticle, need to complete form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,60 @@ function App() {
       </header>
     </div>
   );
+}
+
+export default App;
+*/
+
+/*
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route path='/create-book' component={CreateBook} />
+          <Route path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;*/
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import CreateArticle from './components/CreateArticle';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route path='/create-article' component={CreateArticle} />
+          <Route path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
