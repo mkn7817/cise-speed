85d83973cb51cb84f3588080f5a7df5c5f03c58c
mkn7817 <mkn7817@autuni.ac.nz>
2022-09-28 08:13:58 +1300

    committing to test branch

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+
 // app.js
 
 const express = require('express');
@@ -13,3 +14,34 @@ app.get('/', (req, res) => res.send('Hello world!'));
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+/*
+// app.js
+
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+*/
