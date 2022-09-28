4d4f8fdcaafeaad1b3b65ffe63b8d58e820aaa85
Joseph Kim <kimg009263@gmail.com>
2022-09-28 17:02:23 +1300

    heroku features updated

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ app.use(express.json({ extended: false}));
 
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
     })
 } else {
     app.get('/', (req,res)=>{
